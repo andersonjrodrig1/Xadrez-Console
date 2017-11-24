9f4244a0332a62de218792ce103599fc370fed50
Anderson Junior Rodrigues <andersonkz@ymail.com>
2017-11-24 17:02:01 -0200

    Começando a colocar peças no tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -7,6 +7,7 @@ namespace Tabuleiro
         static void Main(string[] args)
         {
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+            tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
 
             Tela.imprimirTabuleiro(tabuleiro);
 
---
+++ Xadrez-Console/Xadrez/Torre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    public class Torre : Peca
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
