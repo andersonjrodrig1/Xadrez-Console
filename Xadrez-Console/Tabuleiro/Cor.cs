be064a831db0884609fc50eadbcc97a18636edd0
Anderson Junior Rodrigues <andersonkz@ymail.com>
2017-11-23 23:21:45 -0200

    Comecando a implentar o tabuleiro

---
+++ Xadrez-Console/Tabuleiro/Cor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    public enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
